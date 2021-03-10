346ef1667461eb6682916d070dff7b38a997a908
Chang Dae <bnormal16@naver.com>
2021-03-10 17:18:28 +0900

    210310

--- dezero/functions.py
+++ dezero/functions.py
@@ -234,6 +234,17 @@ def linear_simple(x, W, b=None):
     return y
 
 
+# =============================================================================
+# accuracy / dropout / batch_norm / embed_id
+# =============================================================================
+def accuracy(y, t):
+    y, t = as_variable(y), as_variable(t)
+
+    pred = y.data.argmax(axis=1).reshape(t.shape)
+    result = (pred == t.data)
+    acc = result.mean()
+    return Variable(as_array(acc))
+
 
 #--------------------------------------------------------------------------------------
 # Activation (Sigmoid, softmax)
