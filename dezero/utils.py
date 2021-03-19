50b5292ccad9ae6941de8d52afae5afe7fade264
Chang Dae <bnormal16@naver.com>
2021-03-19 17:50:26 +0900

    210319

--- dezero/utils.py
+++ dezero/utils.py
@@ -190,3 +190,12 @@ def pair(x):
         return x
     else:
         raise ValueError
+        
+        
+# =============================================================================
+# for Convolution
+# =============================================================================  
+        
+        
+def get_conv_outsize(input_size, kernel_size, stride, pad):
+    return (input_size + pad * 2 - kernel_size) // stride + 1
